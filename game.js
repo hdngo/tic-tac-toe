5de932dd6ede56e2c1a52402aedcc70232402709
Harvey Ngo <harveydngo@gmail.com>
2015-08-23 15:30:17 -0700

    add reset score functionality and score update functionality, work on refactoring later

--- game.js
+++ game.js
@@ -1,10 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+	var gameOver = false;
 	var turn = 0;
+
+	var playerX = {
+		name: "X",
+		score: 0
+	};
+	var playerO = {
+		name: "O",
+		score: 0
+	};
+	var currentPlayer = playerX;
+
+	var playerXScore = document.getElementById('X');
+	var playerOScore = document.getElementById('O');
+	
 	var squares = document.getElementsByClassName('square')
+
 	var newGameButton = document.getElementById('new-game-btn')
 	newGameButton.addEventListener('click', newGame)
-	var gameOver = false;
+
+	var resetButton = document.getElementById('reset-btn')
+	resetButton.addEventListener('click', resetScores)
+
 
 	var assortedSquares = [[squares[0], squares[1], squares[2]], [squares[3], squares[4], squares[5]], [squares[6], squares[7], squares[8]]]
 
@@ -16,22 +35,23 @@ document.addEventListener("DOMContentLoaded", function(){
 		if (gameOver){
 			return
 		}
-		checkSquare(event.target);
-		if (checkSquare(event.target)){
+		if (checkSquare(this)){
 			alert("that square has been taken already, pick another one!")
 		}
 		else{
-		if (turn % 2 == 0){
-			marker = "X";
-		}
-		else {
-			marker = "O";
-		}
+			if (turn % 2 == 0){
+				marker = "X";
+				currentPlayer = playerX;
+			}
+			else {
+				marker = "O";
+				currentPlayer = playerO;
+			}
 			playerMarker = makePlayerMarker(marker)
-			event.target.appendChild(playerMarker);
-			event.target.classList.add('taken');
+			this.appendChild(playerMarker);
+			this.classList.add('taken');
 			checkForWin();
-		turn++;
+			turn++;
 		}
 	}
 
@@ -102,12 +122,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	function checkForWin(){
 		if(checkLeftDiagonal() || checkRightDiagonal()){
-			alert("we have a winner")
+			alert("we have a winner");
+			updateWins();
 			gameOver = true;
 		}
 		for (var rowColIndex = 0; rowColIndex < assortedSquares.length; rowColIndex++){
 			if(checkRow(assortedSquares[rowColIndex]) || checkColumn(rowColIndex)){
 				alert("we have a winner")
+				updateWins();
 				gameOver = true;
 			}
 		}
@@ -118,6 +140,21 @@ document.addEventListener("DOMContentLoaded", function(){
 			squares[cellIndex].innerText = '';
 			squares[cellIndex].classList.remove('taken');
 		}
+		gameOver = false;
+	}
+
+	function updateWins(){
+		currentPlayer.score++;
+		playerXScore.innerText = playerX.score;
+		playerOScore.innerText = playerO.score;
+	}
+
+	function resetScores(){
+		playerX.score = 0;
+		playerO.score = 0;
+		playerXScore.innerText = playerX.score;
+		playerOScore.innerText = playerO.score;
+		newGame();
 	}
 
 });
