be1c0d26dd738aac55e23675fa61ad8678abd877
Harvey Ngo <harveydngo@gmail.com>
2015-09-07 01:05:58 -0700

    reimplement sort board, check row-column-diagonal functions, add simulate game move function that creates a copy of the current game for later implementation of recursive calls to obtain move scores

--- unbeatable-ttt.js
+++ unbeatable-ttt.js
@@ -43,19 +43,25 @@ document.addEventListener("DOMContentLoaded", function(){
 			alert('invalid move')
 		}
 	}
-
+	console.log(newGame.sortedBoard)
 	
 });
+
+var simulationCount = 0;
+////////////////////////////////////////////
+
+////////////////////////////////////////////
 
 // game objects
 
 function Game(board){
-	this.winner = null;
+	this.winner,
 	this.turn = 0;
 	this.board = board,
 	this.humanPlayer = "X",
 	this.computerPlayer = "O",
-	this.currentPlayer = this.humanPlayer;
+	this.currentPlayer = this.humanPlayer,
+	this.sortedBoard = this.sortBoard(board)
 }
 
 Game.prototype.switchPlayers = function(){
@@ -75,8 +81,9 @@ Game.prototype.makeComputerMove = function(
 	availableMoves = this.getAvailableMoves();
 	console.log(availableMoves)
 	//currentPlayer, squareIndex){
-
-
+	this.simulate(new Game(this.board), this.currentPlayer)
+	console.log('no winner')
+	console.log(this.winner)
 	// console.log(currentPlayer) 
 	// marker = document.createTextNode(currentPlayer)
 	// this.board[squareIndex].appendChild(marker)
@@ -101,10 +108,52 @@ Game.prototype.getAvailableMoves = function(){
 	return availableMoves
 }
 
+Game.prototype.simulate = function(simGame, currentPlayer){
+	simGame.winner = true;
+	console.log('fake win')
+	console.log(simGame.winner)
+	console.log(simGame.getAvailableMoves())
+}
+
 Game.prototype.makeMove = function (game, player){
 	marker = document.createTextNode(player)	
 	console.log(player)
 	this.board[1].appendChild(marker)
 }
 
+Game.prototype.sortBoard = function(){
+	sortedGameBoard = [[this.board[0], this.board[1], this.board[2]], [this.board[3], this.board[4], this.board[5]], [this.board[6], this.board[7], this.board[8]]]
+	return sortedGameBoard
+}
+
+Game.prototype.checkThreeCells = function(arrayOfThreeCells, currentPlayer){
+	if(arrayOfThreeCells[0].innerText === currentPlayer && arrayOfThreeCells[1].innerText === currentPlayer && arrayOfThreeCells[2].innerText === currentPlayer){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+Game.prototype.checkRow = function(rowIndex){
+	return checkThreeCells(row)
+}
+
+Game.prototype.checkColumn = function(ColumnIndex, sortedGameBoard){
+	var column = [];
+	for(var rowNumber = 0; rowNumber < sortedGameBoard.length; rowNumber++){
+		column.push(assortedBoard[rowNumber][columnNumber])
+	}
+	return checkThreeCells(column)
+}
+
+Game.prototype.checkLeftDiagonal = function(sortedGameBoard){
+	var leftDiagonal = [sortedGameBoard[0][0], sortedGameBoard[1][1], sortedGameBoard[2][2]]
+	return checkThreeCells(leftDiagonal)
+}
+
+Game.prototype.checkRightDiagonal = function(sortedGameBoard){
+	var rightDiagonal = [sortedGameBoard[0][2], sortedGameBoard[1][1], sortedGameBoard[2][0]]
+	return checkThreeCells(rightDiagonal)
+}
 
