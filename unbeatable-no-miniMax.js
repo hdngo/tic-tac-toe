720891fde474098cd5edf67678c46fa9f9c607cf
Harvey Ngo <harveydngo@gmail.com>
2015-09-09 14:52:39 -0700

    take a break from implementing minimax, attempt alternate solution to see if I can implement a non-algorithmic solution for now to practice more javascript

--- unbeatable-no-miniMax.js
+++ unbeatable-no-miniMax.js
@@ -8,11 +8,8 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 	var newGame = new Game(squares);
-	// newGame.makeComputerMove(newGame.currentPlayer, 1);
 	for(var squareNumber = 0; squareNumber < squares.length; squareNumber ++){
 		squares[squareNumber].addEventListener('click', makePlayerMove);
-
-
 	}
 
 	function makePlayerMove(){
@@ -23,13 +20,6 @@ document.addEventListener("DOMContentLoaded", function(){
 			if(this.innerText === ''){
 				letter = document.createTextNode(newGame.currentPlayer)
 				this.appendChild(letter)
-				// newGame.checkForWinner();
-				// newGame.checkForTie();
-				// if(newGame.winner || newGame.Tie){
-					// return
-				// }
-				// else{
-
 				//make the player move, then check to see if there's a win, if there is, 
 				//don't execute the computer's move
 				flattenedBoard = generateFlattenedDomBoard(newGame.board)
@@ -42,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function(){
 					newGame.tie = true;
 					return
 				}
-
 				//if there is no winner after the player's move, execute the computer's
 				else{
 				newGame.switchPlayers();
@@ -74,7 +63,6 @@ function Game(board){
 	this.humanPlayer = "X",
 	this.computerPlayer = "O",
 	this.currentPlayer = this.humanPlayer
-	// this.sortedBoard = this.sortBoard(board)
 }
 
 Game.prototype.switchPlayers = function(){
@@ -111,29 +99,6 @@ Game.prototype.makeComputerMove = function(
 		console.log('SCORE SCORE SCORE')
 		console.log(availableMovesWithScores)
 	}
-	// console.log('scores')
-	// console.log(availableMovesWithScores);
-
-
-	
-
-	// for(var index = 0; index< flattenedBoard.length; index++){
-		// console.log(flat)
-	// }
-
-	// availableMoves = this.getAvailableMoves();
-	// availableMovesAndScores = this.getMoveScores(availableMoves)
-	// for(var possibleMove = 0; possibleMove < availableMoves.length; possibleMove++){
-	// 		console.log('return value')
-	// 		//go through each move and update the score
-	// 		//return the updated game for each, call the check function on it, otherwise repeat the 
-	// 		//process
-	// 		simulatedGame = this.simulateMove(new Game(this.board), availableMoves[possibleMove], this.currentPlayer)
-	// 		scoreForSimulatedGame = simulatedGame.getSimulatedMoveScore(simulatedGame)
-	// 		console.log(scoreForSimulatedGame)
-	// debugger
-	// }
-	// this.switchPlayers();
 }
 
 
@@ -152,7 +117,6 @@ function miniMax(simulatedBoard, moveIndex, playerMark, turn){
 		}
 	}
 	else{
-		// console.log('no winner mr.beginner ' + playerMark)
 		//loop thru the available moves, grab the scores for the next ones, and based on the player, pick the score
 		console.log('turn number ' + turn)
 		console.log(simulatedBoard)
@@ -175,157 +139,6 @@ function miniMax(simulatedBoard, moveIndex, playerMark, turn){
 	}
 
 }
-
-// Game.prototype.getAvailableMoves = function(){
-// 	availableMoves = [];
-// 	for(var availableMoveIndex = 0; availableMoveIndex < this.board.length; availableMoveIndex++){
-// 		if(this.board[availableMoveIndex].innerText === ''){
-// 			availableMoves.push(availableMoveIndex)
-// 		}
-// 	}
-// 	return availableMoves
-// }
-
-// for in starts at 0
-// Game.prototype.getMoveScores = function(availableMoves){
-// 	moveScores = {}
-// 	for(var availableMoveIndex = 0; availableMoveIndex< availableMoves.length; availableMoveIndex++){
-// 		moveScores[availableMoves[availableMoveIndex]] = 0
-// 	}
-// 	return moveScores
-// }
-
-//have a simulator function that returns a game object, check if there's a value from that
-	//the simulator function makes the move and calls the check for winner/tie and returns the game with the update status so that we can call win/loss
-
-// Game.prototype.getSimulatedMoveScore = function(simulatedGame){
-// 	//possibly group checks into a game over function
-// 	if(simulatedGame.isOver()){
-// 		if(simulatedGame.winner === simulatedGame.humanPlayer){
-// 			return 10
-// 		}
-// 		else if(simulatedGame.winner === simulatedGame.computerPlayer){
-// 			return -10
-// 		}
-// 		else if(simulatedGame.tie){
-// 			console.log('tie')
-// 			return 0
-// 		}
-// 	}
-// 	else{
-// 		console.log('hi')
-// 		console.log(simulatedGame.currentPlayer)
-// 		nextPossibleMoves = simulatedGame.getAvailableMoves();
-// 		console.log(nextPossibleMoves)
-// 		for(var index = 0; index < nextPossibleMoves.length; index++){
-// 			console.log('simulate move for this player')
-// 			console.log(simulatedGame.currentPlayer)
-// 			childGame = simulatedGame.simulateMove(new Game(simulatedGame.board), nextPossibleMoves[index], simulatedGame.currentPlayer)
-// 			childGameScore = simulatedGame.getSimulatedMoveScore(childGame)
-// 			console.log(childGameScore)
-// 			console.log('original board')
-// 			console.log(simulatedGame.board)
-// 		}
-// 		// x = x.map(function(num){ return 2})
-// 		// y = Object.assign({"key": x[1]})
-
-// 	}
-// }
-
-// Game.prototype.simulateMove = function(parentGame, moveIndex, currentPlayer){
-// 	// console.log(moveIndex)
-// 	// console.log(currentPlayer)
-// 	console.log('add this marker')
-// 	console.log(currentPlayer)
-// 	parentGame.currentPlayer = currentPlayer;
-// 	parentGame.board[moveIndex].innerText = currentPlayer
-// 	parentGame.checkForWinner();
-// 	parentGame.checkForTie();
-// 	// console.log(parentGame.board)
-// 	return new Game(parentGame.board)
-// }
-
-// Game.prototype.isOver = function(){
-// 	if(this.winner || this.tie){
-// 		return true
-// 	}
-// 	else{
-// 		return false
-// 	}
-// }
-
-// Game.prototype.makeMove = function (game, player){
-// 	marker = document.createTextNode(player)	
-// 	this.board[1].appendChild(marker)
-// }
-
-// Game.prototype.sortBoard = function(){
-// 	sortedGameBoard = [[this.board[0], this.board[1], this.board[2]], [this.board[3], this.board[4], this.board[5]], [this.board[6], this.board[7], this.board[8]]]
-// 	return sortedGameBoard
-// }
-
-// Game.prototype.checkThreeCells = function(arrayOfThreeCells){
-// 	if(arrayOfThreeCells[0].innerText === this.currentPlayer && arrayOfThreeCells[0].innerText === arrayOfThreeCells[1].innerText && arrayOfThreeCells[1].innerText === arrayOfThreeCells[2].innerText){
-// 		return true
-// 	}
-// 	else{
-// 		return false;
-// 	}
-// }
-
-// Game.prototype.checkRow = function(rowNumber, sortedGameBoard){
-// 	row = sortedGameBoard[rowNumber]
-// 	return this.checkThreeCells(row)
-// }
-
-// Game.prototype.checkColumn = function(columnNumber, sortedGameBoard){
-// 	var column = [];
-// 	for(var rowNumber = 0; rowNumber < sortedGameBoard.length; rowNumber++){
-// 		column.push(sortedGameBoard[rowNumber][columnNumber])
-// 	}
-// 	return this.checkThreeCells(column)
-// }
-
-// Game.prototype.checkLeftDiagonal = function(sortedGameBoard){
-// 	var leftDiagonal = [sortedGameBoard[0][0], sortedGameBoard[1][1], sortedGameBoard[2][2]]
-// 	return this.checkThreeCells(leftDiagonal)
-// }
-
-// Game.prototype.checkRightDiagonal = function(sortedGameBoard){
-// 	var rightDiagonal = [sortedGameBoard[0][2], sortedGameBoard[1][1], sortedGameBoard[2][0]]
-// 	return this.checkThreeCells(rightDiagonal)
-// }
-
-// Game.prototype.checkForTie = function(){
-// 	count = 0;
-// 	for(var squareIndex =0; squareIndex < this.board.length; squareIndex++){
-// 		if(this.board[squareIndex].innerText !== ''){
-// 			count++;
-// 		}
-// 	}
-// 	if(count === 9 && !this.checkForWinner()){
-// 		this.tie = true;
-// 		return true;
-// 	}
-// 	else if(count !== 9){
-// 		return false
-// 	}
-// }
-
-// Game.prototype.checkForWinner = function(){
-// 	if(this.checkLeftDiagonal(this.sortedBoard) || this.checkRightDiagonal(this.sortedBoard)){
-// 		this.winner = this.currentPlayer;
-// 		return true
-// 	}
-// 	for(var index = 0; index < this.sortedBoard.length; index++){
-// 		if(this.checkRow(index, this.sortedBoard) || this.checkColumn(index, this.sortedBoard)){
-// 			this.winner = this.currentPlayer
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
-
 
 ////////
 function isNotEmpty(cell, index, array){
@@ -352,14 +165,12 @@ function checkThreeCells(arrayOfThreeCells){
 		}
 	}
 	else{
-		// console.log('some are empty')
 		return false
 	}
 }
 
 function checkRow(rowNumber, sortedGameBoard){
 	row = sortedGameBoard[rowNumber]
-	// console.log(row)
 	return checkThreeCells(row)
 }
 
@@ -414,11 +225,6 @@ function checkForWinner(board){
 		return checkRightDiagonal(sortedBoard)
 	}
 	for(var rowColIndex = 0; rowColIndex < sortedBoard.length; rowColIndex++){
-		// if(checkRow(rowColIndex, sortedBoard) || checkColumn(rowColIndex, sortedBoard)){
-		// 	alert('we have a winner')
-		// 	return true
-		// }
-
 		if(checkRow(rowColIndex, sortedBoard)){
 			return checkRow(rowColIndex,sortedBoard)
 		}
